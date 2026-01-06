1d24a8f0881fe71c7c7169be3a5e7a23235862c2
Masruroh Aris <masruroharis@example.com>
2026-01-06 18:22:11 +0700

    Tambah css pada login

--- login.php
+++ login.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
-<html>
+<html lang="id">
 <head>
+    <meta charset="UTF-8">
     <title>Login Admin</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h2>Login Admin Bengkel</h2>
-    <form action="proses_login.php" method="post">
-        <input type="text" name="username" placeholder="Username" required><br><br>
-        <input type="password" name="password" placeholder="Password" required><br><br>
-        <button type="submit">Login</button>
-    </form>
+
+    <div class="login-container">
+        <h2>Login Admin Bengkel</h2>
+
+        <form action="proses_login.php" method="post">
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+
 </body>
 </html>
