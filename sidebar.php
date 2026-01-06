86822f42a91175ecc0c84222a819807b2bb1cf40
Masruroh Aris <masruroharis@example.com>
2026-01-06 23:20:08 +0700

    tambah sidebar

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+<aside class="sidebar">
+    <h1 class="logo">BENGKEL</h1>
+
+    <nav>
+        <a href="dashboard.php">🏠 Dashboard</a>
+        <a href="produk.php" class="active">🛠 Data Produk</a>
+        <a href="pelanggan.php">👥 Data Pelanggan</a>
+        <a href="transaksi.php">💳 Transaksi</a>
+        <a href="logout.php" class="logout">🚪 Logout</a>
+    </nav>
+</aside>
